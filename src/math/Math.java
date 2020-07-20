bcf07b4eadcf9998023d8abbe9bf509584e25734
hoanganh211299 <ngothuonghoanganh1144@gmail.com>
2020-07-20 09:51:42 +0700

    abc

--- src/math/Math.java
+++ src/math/Math.java
@@ -18,7 +18,23 @@ public class Math {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //5! hy vong may in ra 120
         System.out.println("5! = " + computerFactories(5));
+
+        //6! hy vong no in ra 720
+        //thuc te in ra bao nhieu phai chay moi biet
+        System.out.println("6! = " + computerFactories(6));
+        //actueal: 720, expected 720 -> correct 
+        //actueal: 1000, expected 720 -> error
+        System.out.println("1! = " + computerFactories(1));
+        //actueal: 1, expected 1 -> correct 
+        //actueal: 720, expected 1 -> errorÏ
+        System.out.println("-5! = " + computerFactories(-5));
+//        ky vong nem ve ngoai le do dua vao du lieu sai do giai thua chi choi voi so duong
+//        dung mau sac neu xanh la ky vong dung neu do la ky vong sai
+//        viet them vao code de nem ra mau
+//        phu tro Junit, Nunit, testNG, PHPunit, CPPunit, ...
+//        chay, F6, nhin ket qua luan dung sai bang mat tren tung ham sout()
     }
-    
+
 }
---
+++ test/tdd/UnitTestTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+import static anh.util.MathUtil.computerFactories;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author ngoanh
+ */
+public class UnitTestTest {
+    
+    // day la class dung de test code chinh ben manth util 
+    // khongo xai sout() ma dung mau xanh do
+    // class nay chua rat nhieu ham main
+    
+    // dung @test, JVM
+
+    /**
+     *
+     */
+    
+    @Test // ham main nay dung de test ca tinh huong
+    public void testSuccessfulCasees(){
+//        assertEquals(120, 720);
+        assertEquals(120, computerFactories(5));
+        assertEquals(720, computerFactories(6));
+        assertEquals(720*7, computerFactories(7));
+        assertEquals(2, computerFactories(2));
+//        assertEquals(0, computerFactories(1));
+
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+         public void testFailedCasees(){
+//        assertEquals(120, 720);
+        // ham nay de test nhung tinh huong ca chon den tu nguoi dung
+        // vi dung muon tinh -5! thi khong the thuc hien duoc
+        // khi do trong thiet ke cua ham cF() ta phia du lieu tinh huong nay
+        // tuc la khi nguoi dung dua data ca chon vao thi minh nem ra cai ngoai
+        // le khong can xu li
+        // Va ngoai le la 1 thu bat thuong, khong nhu du tinh, khong phai la 1 value
+        // co the so sanh, tuc la khong the assertEquals(mot-cai-ngoai-le)
+        computerFactories(-5);
+    }
+    public UnitTestTest() {
+    }
+    
+}
